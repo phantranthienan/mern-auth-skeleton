3e1b063c08894fb67fcac78b1c2156d6393db085
Tran Thien An PHAN <phantranthienan1405@gmail.com>
2025-01-29 00:47:34 +0100

    update structure frontend

---
+++ frontend/src/contexts/AppQueryProvider.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
+
+const AppQueryProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+            <ReactQueryDevtools initialIsOpen={true}/>
+        </QueryClientProvider>
+    )
+};
+
+export default AppQueryProvider;
